d03865f15d637ca4abda91cf6e019a8263af9982
Stephan Kulla <git.mail@kulla.me>
2025-04-26 18:03:37 +0200

    chore: Add HTML title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  html: {
+    title: 'React Textarea with ContentEditable',
+  },
   output: {
     assetPrefix: '/2025-04-26-react-textarea-with-contenteditable/',
   },
