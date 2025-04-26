6014ee42ecbc2576c47eb99ae088219bb9e0ca0a
Stephan Kulla <git.mail@kulla.me>
2025-04-26 18:08:09 +0200

    feat: Basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import '@picocss/pico/css/pico.min.css'
 import './App.css'
 
-const App = () => {
+export default function App() {
   return (
     <main className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Textarea</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren
+      </p>
     </main>
   )
 }
-
-export default App
